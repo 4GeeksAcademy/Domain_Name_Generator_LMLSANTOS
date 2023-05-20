8002306dd4e581b5990d797db808b7c55d27a630
lmlsantos <126293567+lmlsantos@users.noreply.github.com>
2023-05-20 16:32:27 +0000

    LMLSANTOS: Domain Name Generator Corrected

--- src/app.js
+++ src/app.js
@@ -1,22 +1,23 @@
 let generateRandomDomain = () => {
-  let pronoun = ['the','our', 'his', 'her', 'ours', 'theirs', 'their'];
-  let adj = ['great', 'big', 'Small', 'Magnificient', 'Awesome' ];
-  let noun = ['jogger','racoon', 'Honor', 'Pack', 'ocean', 'godfather', 'player'];
-  let extension =['com', 'net', 'pt', 'es', 'us', 'io'];
+  let pronoun = ["the", "our", "his", "her", "ours", "theirs", "their"];
+  let adj = ["great", "big", "Small", "Magnificient", "Awesome"];
+  let noun = [
+    "jogger",
+    "racoon",
+    "Honor",
+    "Pack",
+    "ocean",
+    "godfather",
+    "player"
+  ];
+  let extension = ["com", "net", "pt", "es", "us", "io"];
 
-  let domainNames = [];
-
-  for (let i=0; i<pronoun.length; i++){
-    for (let j=0; j<adj.length; j++){
-      for (let k=0; k<noun.length; k++){
-        for (let l=0; l<extension.length; l++){
-          const domainName = `${pronoun[i]}${adj[j]}${noun[k]}.${extension[l]}`;
-          domainNames.push(domainName);
-        }
-      }
-    }
-  }
-  const randomIndex = Math.floor(Math.random()*domainNames.length);
-  return domainNames[randomIndex];
+  return (
+    pronoun[Math.floor(Math.random() * pronoun.length)] +
+    adj[Math.floor(Math.random() * adj.lengt)] +
+    noun[Math.floor(Math.random() * noun.length)] +
+    "." +
+    extension[Math.floor(Math.random() * extension.length)]
+  );
 };
 console.log(generateRandomDomain());
